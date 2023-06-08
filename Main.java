eb7336cc49d2330a45068bdc1f7ba95308918e3d
DhaNaBoi <shammidhananjaya@gmail.com>
2023-06-09 01:14:03 +0530

    resetGame + MainMethod + Code Restructure

--- Main.java
+++ Main.java
@@ -1,110 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 
-public class Main {
-    private static final int BOARD_SIZE = 100;
-
-    private JFrame frame;
-    private JPanel boardPanel;
-    private JLabel[] cells;
-    private JLabel diceValueLabel;
-    private JLabel[] playerLabels;
-    private JButton rollDiceButton;
-    private JButton resetButton;
-
+class Game {
     private Board board;
     private Player[] players;
-    private Game game;
+    private int currentPlayerIndex;
 
-
-
-
-        public Main() {
-        initializeBoard();
-        initializePlayers();
-        initializeGame();
-        createInterface();
+    public Game(Board board, Player[] players) {
+        this.board = board;
+        this.players = players;
+        this.currentPlayerIndex = 0;
     }
-
-    private void initializeBoard() {
-        board = new Board(BOARD_SIZE);
-        board.addLadder(10, 25);
-        board.addLadder(16, 33);
-        board.addLadder(30, 55);
-        board.addLadder(45,70);
-
-
-        board.addSnake(17, 7);
-        board.addSnake(54, 34);
-        board.addSnake(62, 19);
-        board.addSnake(88,61);
-
-    }
-
-    private void initializePlayers() {
-        players = new Player[2];
-        players[0] = new Player("Player 1");
-        players[1] = new Player("Player 2");
-    }
-        private void initializeGame() {
-            game = new Game(board, players);
-        }
-
-        private void createInterface() {
-
-            JPanel controlPanel = new JPanel();
-            controlPanel.add(diceValueLabel);
-            controlPanel.add(rollDiceButton);
-            controlPanel.add(resetButton);
-
-            frame.getContentPane().add(boardPanel, BorderLayout.CENTER);
-            frame.getContentPane().add(infoPanel, BorderLayout.WEST);
-            frame.getContentPane().add(controlPanel, BorderLayout.SOUTH);
-            frame.setSize(500, 500);
-            frame.setVisible(true);
-
-
-            JPanel infoPanel = new JPanel(new GridLayout(2, 1));
-            playerLabels = new JLabel[players.length];
-            for (int i = 0; i < players.length; i++) {
-                JLabel playerLabel = new JLabel(players[i].getName() + ": 0");
-                playerLabels[i] = playerLabel;
-                infoPanel.add(playerLabel);
-            }
-
-            diceValueLabel = new JLabel("Dice Value: ");
-            rollDiceButton = new JButton("Roll Dice");
-            rollDiceButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    int diceRoll = game.rollDice();
-                    diceValueLabel.setText("Dice Value: " + diceRoll);
-                    game.play();
-                    updatePlayerPositions();
-                }
-            });
-
-
-            resetButton = new JButton("Reset");
-            resetButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    resetGame();
-                }
-            });
-        }
-
-    class Game {
-        private Board board;
-        private Player[] players;
-        private int currentPlayerIndex;
-
-        public Game(Board board, Player[] players) {
-            this.board = board;
-            this.players = players;
-            this.currentPlayerIndex = 0;
-        }
 
     public void play() {
         Player currentPlayer = players[currentPlayerIndex];
@@ -137,26 +47,123 @@ public class Main {
     }
 }
 
+public class Main {
+    private static final int BOARD_SIZE = 100;
+
+    private JFrame frame;
+    private JPanel boardPanel;
+    private JLabel[] cells;
+    private JLabel diceValueLabel;
+    private JLabel[] playerLabels;
+    private JButton rollDiceButton;
+    private JButton resetButton;
+
+    private Board board;
+    private Player[] players;
+    private Game game;
+
+    public Main() {
+        initializeBoard();
+        initializePlayers();
+        initializeGame();
+        createInterface();
+    }
+
+    private void initializeBoard() {
+        board = new Board(BOARD_SIZE);
+        board.addLadder(10, 25);
+        board.addLadder(16, 33);
+        board.addLadder(30, 55);
+        board.addLadder(45,70);
 
 
-    private void updatePlayerPositions() {
-            for (int i = 0; i < players.length; i++) {
-                Player player = players[i];
-                int position = player.getPosition();
-                cells[position - 1].setText(player.getName());
-                playerLabels[i].setText(player.getName() + ": " + position);
+        board.addSnake(17, 7);
+        board.addSnake(54, 34);
+        board.addSnake(62, 19);
+        board.addSnake(88,61);
+
+    }
+
+    private void initializePlayers() {
+        players = new Player[2];
+        players[0] = new Player("Player 1");
+        players[1] = new Player("Player 2");
+    }
+    
+    private void initializeGame() {
+        game = new Game(board, players);
+    }
+
+    private void createInterface() {
+
+        JPanel infoPanel = new JPanel(new GridLayout(2, 1));
+        playerLabels = new JLabel[players.length];
+        for (int i = 0; i < players.length; i++) {
+            JLabel playerLabel = new JLabel(players[i].getName() + ": 0");
+            playerLabels[i] = playerLabel;
+            infoPanel.add(playerLabel);
+        }
+
+        diceValueLabel = new JLabel("Dice Value: ");
+        rollDiceButton = new JButton("Roll Dice");
+        rollDiceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int diceRoll = game.rollDice();
+                diceValueLabel.setText("Dice Value: " + diceRoll);
+                game.play();
+                updatePlayerPositions();
             }
+        });
 
 
+        resetButton = new JButton("Reset");
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetGame();
+            }
+        });
+    
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(diceValueLabel);
+        controlPanel.add(rollDiceButton);
+        controlPanel.add(resetButton);
+
+        frame.getContentPane().add(boardPanel, BorderLayout.CENTER);
+        frame.getContentPane().add(infoPanel, BorderLayout.WEST);
+        frame.getContentPane().add(controlPanel, BorderLayout.SOUTH);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+
+    
+    private void updatePlayerPositions() {
+        for (int i = 0; i < players.length; i++) {
+            Player player = players[i];
+            int position = player.getPosition();
+            cells[position - 1].setText(player.getName());
+            playerLabels[i].setText(player.getName() + ": " + position);
         }
+    }
 
-        private void resetGame() {
-
+    private void resetGame() {
+        for (Player player : players) {
+            player.setPosition(0);
         }
-
-        public static void main(String[] args) {
-
+        diceValueLabel.setText("Dice Value: ");
+        for (int i = 0; i < cells.length; i++) {
+            cells[i].setText(Integer.toString(i + 1));
         }
-    });
-}
+        updatePlayerPositions();
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+    }
 }
